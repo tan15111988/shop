b637cb0021eb25afb386a2e787d6f5f0898c122a
TanNN <tan15111988@gmail.com>
2013-06-17 23:58:27 +0700

    First commit

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:81/shop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:81/shop/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\shop/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\shop/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\shop/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\shop/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\shop/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\shop/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\shop/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\shop/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\shop/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\shop/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop');
+define('DB_PREFIX', 'oc_');
+?>
